a8b74a1251aeccfbd76c9a4fb703f94f7edd5071
kissa1001 <kissa101295@gmail.com>
2016-02-18 22:59:25 -0500

    velocity.js added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-function(){
+$(function(){
 
-};
+$("#play").mouseenter(function(){
+	$(this).velocity( { translateY: "-10px", backgroundColor: "#FF7376" }, 400 );
+});
+
+$("#play").mouseleave(function(){
+	$(this).velocity( { translateY: "0", backgroundColor: "#FF2C2E" }, 200 );
+});
+
+$("#play").mousedown(function(){
+	$(this)
+		.velocity({ translateY: "-30px", rotateZ: "10deg" }, 100, "easeOut")
+		.velocity({ rotateZ: "-8deg" }, 150)
+		.velocity({ translateY: "0", rotateZ: "0" }, {duration: 600, easing: [ 500, 14 ]});
+});
+
+});
