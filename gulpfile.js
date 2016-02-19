e8529446d1aab66cc668b1183319c6ea4851789f
kissa1001 <kissa101295@gmail.com>
2016-02-18 20:11:35 -0500

    get sassy

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
@@ -10,53 +9,62 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var neat = require('node-neat');
+
 // JavaScript linting task
 gulp.task('jshint', function() {
-  return gulp.src('site/js/*.js')
+    return gulp.src('site/js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
+
 // Compile Sass task
 gulp.task('sass', function() {
-  return gulp.src('site/scss/*.scss')
+    return gulp.src('site/scss/*.scss')
     .pipe(sass({
-      includePaths: require('node-bourbon').includePaths 
+        includePaths: neat.includePaths
     }))
     .pipe(gulp.dest('site/css'));
 });
+
 gulp.task('watch', function() {
-  gulp.watch('site/js/*.js', ['jshint']);
-  gulp.watch('site/scss/*.scss', ['sass']);
+    gulp.watch('site/js/*.js', ['jshint']);
+    gulp.watch('site/scss/*.scss', ['sass']);
 });
+
 // Default task
 gulp.task('default', ['jshint', 'sass', 'watch']);
 
 // Minify index
 gulp.task('html', function() {
-  return gulp.src('site/index.html')
+    return gulp.src('site/index.html')
     .pipe(minifyHTML())
     .pipe(gulp.dest('build/'));
 });
+
 // JavaScript build task, removes whitespace and concatenates all files
 gulp.task('scripts', function() {
-  return browserify('site/js/main.js')
+    return browserify('site/js/main.js')
     .bundle()
     .pipe(source('app.js'))
     .pipe(buffer())
     .pipe(uglify())
     .pipe(gulp.dest('build/js'));
 });
+
 // Styles build task, concatenates all the files
 gulp.task('styles', function() {
-  return gulp.src('site/css/*.css')
+    return gulp.src('site/css/*.css')
     .pipe(concat('style.css'))
     .pipe(gulp.dest('build/css'));
 });
+
 // Image optimization task
 gulp.task('images', function() {
-  return gulp.src('site/img/*')
+    return gulp.src('site/img/*')
     .pipe(imagemin())
     .pipe(gulp.dest('build/img'));
 });
+
 // Build task
 gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images']);
